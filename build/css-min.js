b1b08ada00b1c6230fb0ed84e3c4fb35d2b4c7ed
Piyamas Kolla <piyamaskolla@outlook.com>
2018-11-16 05:52:40 +0000

    fix cssnano error

--- build/css-min.js
+++ build/css-min.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 let file = path.resolve('css', 'website.css');
-cssnano(fs.readFileSync(file)).then(result => {
+cssnano(fs.readFileSync(file), {from: undefined}).then(result => {
   let filemin = path.resolve('css', 'website.min.css');
   fs.writeFileSync(filemin, result.css);
 });
